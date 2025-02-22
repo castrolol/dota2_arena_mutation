0b2e78efcc2ce37e794ce8bef21c2786225ecd55
Luan Castro Aleixo <luan.aleixo@stone.com.br>
2025-02-22 06:51:08 -0400

    Adding toggling panel

--- content/panorama/src/hud/abilities/AbilityHud.tsx
+++ content/panorama/src/hud/abilities/AbilityHud.tsx
@@ -1,13 +1,28 @@
-import { AbilityHudHeader } from "./AbilityHudHeader";
-import { AbilityHudPaths } from "./AbilityHudPath";
+import { useEffect, useState } from 'react';
+import { AbilityHudHeader } from './AbilityHudHeader';
+import { AbilityHudPaths } from './AbilityHudPath';
+import clsx from '../../utils/clsx';
 
 export function AbilityHud() {
+    const [opened, setOpened] = useState(false);
+
+    useEffect(() => {
+        $.RegisterKeyBind($.GetContextPanel(), 'key_escape', () => {
+            $.Msg('Apertou');
+        });
+    }, []);
 
     return (
-        <Panel className="AbilityHud-root">
-            <AbilityHudHeader />
-            <AbilityHudPaths />
-        </Panel>
-    )
+        <>
+            <Panel className={clsx('AbilityHud-root', { open: opened })}>
+                <AbilityHudHeader />
+                <AbilityHudPaths />
+            </Panel>
+            <AbilityHudToggle opened={opened} onClick={() => setOpened(!opened)} />
+        </>
+    );
+}
 
+export function AbilityHudToggle({ opened, onClick }: { opened: boolean; onClick: () => void }) {
+    return <Panel onactivate={onClick} className={clsx('AbilityHud-toggle', { open: opened })}></Panel>;
 }
